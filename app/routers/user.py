c24a552bddc4ccc1a57598841a55a194f2f804f2
HarshalPawar88 <harshaljpawar88@gmail.com>
2022-06-28 19:07:38 +0530

    made changes to for solving /docs issue

--- app/routers/user.py
+++ app/routers/user.py
@@ -5,9 +5,10 @@ from ..database import get_db
 
 router= APIRouter(
     prefix="/users",
-     tags=['Posts']
+     tags=['Users']
 )
 
+#@router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.UserOut)
 def create_users(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
--- app/schemas.py
+++ app/schemas.py
@@ -47,14 +47,6 @@ class UserCreate(BaseModel):
     email: EmailStr
     password: str
 
-class UserOut(BaseModel):
-    id: int
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        orm_mode=True
-
 class UserLogin(BaseModel):
     email: EmailStr
     password: str
